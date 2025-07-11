3b3953b4079d75e4df879d2e982fced35af54d38
wapiiqo <muhammadwafiq1313@gmail.com>
2025-07-12 01:35:35 +0700

    revisi src

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
-import express from "express";
-import RumahRoutes from "./route/rumah";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-app.use(RumahRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === "GET") {
+    res.status(200).json({ message: "Data rumah (GET)" });
+  } else if (req.method === "POST") {
+    res.status(201).json({ message: "Data rumah ditambahkan (POST)" });
+  } else {
+    res.status(405).json({ message: "Method tidak diizinkan" });
+  }
+}
